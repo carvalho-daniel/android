bc150ba4228eebebde9ef9f91bfcb6aef0510407
Daniel.java <daniel.scarvalho05@gmail.com>
2025-02-19 10:00:39 -0300

    mudança de shared preferences para base de dados firebase

--- Login/app/src/main/java/com/example/login/Cadastro.java
+++ Login/app/src/main/java/com/example/login/Cadastro.java
@@ -3,20 +3,33 @@ package com.example.login;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Cadastro extends AppCompatActivity {
 
     Button cadastrar;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,9 @@ public class Cadastro extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
 
         cadastrar = findViewById(R.id.btnCadastrar);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
 
         cadastrarUsuario();
     }
@@ -38,24 +54,54 @@ public class Cadastro extends AppCompatActivity {
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText edtNome = findViewById(R.id.nomeCad);
+                EditText edtNome  = findViewById(R.id.nomeCad);
+                EditText edtIdade = findViewById(R.id.idadeCad);
+                EditText edtEmail = findViewById(R.id.emailCad);
                 EditText edtSenha = findViewById(R.id.senhaCad);
 
                 String nome = edtNome.getText().toString();
+                int idade = Integer.parseInt(edtIdade.getText().toString());
+                String email = edtEmail.getText().toString();
                 String senha = edtSenha.getText().toString();
 
-                if(!nome.isEmpty() && !senha.isEmpty()) {
-                    SharedPreferences shared = getSharedPreferences("acessoUsuario", MODE_PRIVATE);
-                    SharedPreferences.Editor editor =  shared.edit();
+                if(!email.isEmpty() && !senha.isEmpty()) {
+                    mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if(user != null)
+                                    user.sendEmailVerification();
 
-                    editor.putString("nome", nome);
-                    editor.putString("senha", senha);
-                    editor.apply();
+                                DatabaseReference userRef = myRef.child("usuario");
+                                Usuario usr = new Usuario(nome, idade, email);
+
+                                String idUsuario = mAuth.getCurrentUser().getUid();
+
+                                userRef.child(idUsuario).setValue(usr).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.d("Firebase", "Usuário salvo com sucesso");
+                                        } else {
+                                            Log.d("Firebase", "Erro ao tentar salvar usuáiro", task.getException());
+                                        }
+                                    }
+                                });
+
+
+
+                            }
+                        }
+                    });
 
                     Toast.makeText(Cadastro.this, "Registrado com sucesso", Toast.LENGTH_SHORT).show();
 
                     Intent it = new Intent(Cadastro.this, MainActivity.class);
                     startActivity(it);
+                    finish();
+
+
 
                 } else {
                     Toast.makeText(Cadastro.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
@@ -66,4 +112,4 @@ public class Cadastro extends AppCompatActivity {
     }
 
 
-}
+};
